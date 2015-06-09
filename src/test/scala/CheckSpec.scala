458f4730445f16e148ba83299a26d03779433a3c
alltonp <paulallton@gmail.com>
2015-06-09 17:15:12 +0100

    oh yeah, forgotten how good RTDD is  ...

--- src/test/scala/CheckSpec.scala
+++ src/test/scala/CheckSpec.scala
@@ -5,8 +5,35 @@ class CheckSpec extends WordSpec with MustMatchers {
 
   "simple check" in {
     val check = Check(1, "check method in:barry => arg1")
-    check.rawParams mustEqual Array("arg1")
     check.pure mustEqual "check method"
     check.in mustEqual "barry"
+    check.rawParams mustEqual Array("arg1")
+    check.me mustEqual "check/method"
   }
+
+  "simple check with multiple args" in {
+    val check = Check(1, "check method in:barry => arg1 arg2 arg3")
+    check.pure mustEqual "check method"
+    check.in mustEqual "barry"
+    check.rawParams mustEqual Array("arg1", "arg2", "arg3")
+    check.me mustEqual "check/method"
+  }
+
+  "simple check with multiple method bits" in {
+    val check = Check(1, "check a really long method in:barry => arg1")
+    check.pure mustEqual "check a really long method"
+    check.in mustEqual "barry"
+    check.rawParams mustEqual Array("arg1")
+    check.me mustEqual "check/a/really/long/method"
+  }
+
+  "simple check with no args" in {
+    (pending)
+    val check = Check(1, "check method in:barry")
+    check.pure mustEqual "check method"
+    check.in mustEqual "barry"
+    check.rawParams mustEqual Array()
+    check.me mustEqual "check/method"
+  }
+
 }
